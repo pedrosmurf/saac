9b84a7084b58ad8e1f0c2986c7365b04981ae010
Pedro Ferreira <pedro.smurf12345@gmail.com>
2015-02-04 17:20:08 -0300

    atualização dos dados do baco

--- app/models/DB.scala
+++ app/models/DB.scala
@@ -3,6 +3,10 @@ package models
 import scala.slick.jdbc.JdbcBackend.Database._
 import scala.slick.driver.PostgresDriver.simple._
 
-object DB{
-  val connection = Database.forURL(url = "jdbc:postgresql://localhost:5432/saac",user = "saac",password = "s44c", driver="org.postgresql.Driver")
+object DB {
+  val connection = Database.forURL(
+    url = "postgres://flvdzjblusialy:xqilpKZH8rsBPMXnSVR-JXUOcV@ec2-54-235-193-41.compute-1.amazonaws.com:5432/dl94q4dnrenfr",
+    user = "flvdzjblusialy",
+    password = "",
+    driver = "org.postgresql.Driver")
 }
