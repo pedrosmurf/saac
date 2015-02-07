66b61c0a9bdb1bf18ca93c1a9ecfdf190b812a79
Pedro Ferreira <pedro.smurf12345@gmail.com>
2015-02-06 21:33:07 -0300

    correção do banco

--- app/models/DB.scala
+++ app/models/DB.scala
@@ -4,9 +4,8 @@ import scala.slick.jdbc.JdbcBackend.Database._
 import scala.slick.driver.PostgresDriver.simple._
 
 object DB {
-  val connection = Database.forURL(
-    url = "jdbc:postgres://flvdzjblusialy:xqilpKZH8rsBPMXnSVR-JXUOcV@ec2-54-235-193-41.compute-1.amazonaws.com:5432/dl94q4dnrenfr",
+  val connection = Database.forURL(url = "jdbc:postgresql://ec2-54-235-193-41.compute-1.amazonaws.com:5432/dl94q4dnrenfr",
     user = "flvdzjblusialy",
-    password = "",
+    password = "xqilpKZH8rsBPMXnSVR-JXUOcV",
     driver = "org.postgresql.Driver")
 }
